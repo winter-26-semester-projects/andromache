fd7ffe8da81ba2a92ea20821f6a67f34ebe75591
sherifHaraedy <sherifHaraedy@github.com>
2025-12-09 22:57:48 +0200

    finished kernel entry point

---
+++ src/kernel.c
@@ -0,0 +1,13 @@
+#include "kernel/kernel.h"
+#include "config.h"
+
+void kernel_init(void) {
+    (void) CONFIG_MAX_THREADS;
+}
+
+void kernel_main(void) {
+    for (;;) {
+
+        __asm__ volatile ("hlt");
+    }
+}
