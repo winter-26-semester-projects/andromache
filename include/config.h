8d0c31af9764772030bd0e9d2019c4aa1b8cdd78
sherif6931 <sherif6931@github.com>
2025-12-21 04:58:52 +0200

    tweaked formatting

--- include/config.h
+++ include/config.h
@@ -1,8 +1,8 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define CONFIG_MAX_THREADS     64
-#define CONFIG_ENABLE_IPC      1
-#define CONFIG_SCHEDULER_RR    1
+#define CONFIG_MAX_THREADS  64
+#define CONFIG_ENABLE_IPC   1
+#define CONFIG_SCHEDULER_RR 1
 
 #endif /* CONFIG_H */
