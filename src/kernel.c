6ad03ca70b38e7f8550fb970ab0ab8aaec7a792b
sherif6931 <sherif6931@github.com>
2025-12-21 04:58:42 +0200

    todo: update kernel loop

--- src/kernel.c
+++ src/kernel.c
@@ -1,13 +1,15 @@
 #include "kernel/kernel.h"
 #include "config.h"
 
-void kernel_init(void) {
-    (void) CONFIG_MAX_THREADS;
+void kernel_init(void) 
+{
+        (void) CONFIG_MAX_THREADS;
 }
 
-void kernel_main(void) {
-    for (;;) {
+void kernel_main(void) 
+{
+        for (;;) {
 
-        __asm__ volatile ("hlt");
-    }
+            __asm__ volatile ("hlt");
+        }
 }
