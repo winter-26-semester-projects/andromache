c35187ded239903e5fb68d265e9d2e71b1b7fdc7
sherif6931 <sherif6931@github.com>
2025-12-23 02:53:56 +0200

    finished build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,16 @@
 #!usr/bin/env
 
+rm -rf build/
+mkdir -p build
+
+cd ./build/
+cmake ..
+make
+
+cd ..
+
+mkdir -p isodir/boot/grub/
+cp build/kernel.elf isodir/boot/
+cp include/boot/grub.cfg isodir/boot/grub/grub.cfg
+
+grub-mkrescue -o andromache.iso isodir
