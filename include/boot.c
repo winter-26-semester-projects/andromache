f5cfc0a13aaae33d765870cb7d397531660d6a09
sherif6931 <sherif6931@github.com>
2025-12-24 15:37:08 +0200

    finished early boot routine

---
+++ include/boot.c
@@ -0,0 +1,56 @@
+#include "boot/boot.h"
+#include "tty/serial/serial.h"
+
+#define HEADER_OFFSET 0xC0000000
+
+extern struct start header_t;
+
+void early_kernel_init() {
+
+        /* Grabbing header_t entry point */
+        struct start *hdr = (struct start *)((uint32_t)&header_t - HEADER_OFFSET);
+
+        if (hdr->magic != KERNEL_MAGIC) {
+
+                /* It *must* do nothing. */
+                for (;;) {
+
+                        __asm__("hlt");
+                }
+        }
+
+        uint32_t chk = hdr->magic + hdr->kernel_entry + hdr->flags;
+        if (chk != hdr->checksum) {
+
+                /* It again, *must* do nothing. */
+                for (;;) {
+
+                        __asm__("hlt");
+                }
+        }
+
+        if (hdr->flags & 0x1) {
+
+                early_serial_init(0x3F8);
+                early_serial_print("ANDROMACHE Boot: Header Validated.\n");
+        }
+
+        if (hdr->hgr_mem < 0x1000) {
+
+                early_serial_print("ANDROMACHE Boot: Insufficient Memory.\n");
+
+                /* It again, *must* do nothing. */
+                for(;;) {
+
+                        __asm__("hlt");
+                }
+        }
+
+        int32_t entr = hdr->kernel_entry - HEADER_OFFSET;
+
+        /* Grabbing the actual entry point */
+        void (*kernel_main)(void) = (void (*)(void))entr;
+
+        kernel_main();
+}
+
