5068c5bae8d4932c197cb7f8df8a10054efa440c
sherif6931 <sherif6931@github.com>
2025-12-26 13:13:24 +0200

    modified formatting + added documentation

--- include/tty/serial/serial.c
+++ include/tty/serial/serial.c
@@ -1,14 +1,14 @@
-#include "io.h"
+#include "machine/io.h"
 #include "serial.h"
 
 void early_serial_init(uint16_t port) 
 {
-    outb(port + UART_IER, 0x00);    // Disable interrupts
+    outb(port + UART_IER, 0x00);          // Disable interrupts
     outb(port + UART_LCR, UART_LCR_DLAB); // Enable DLAB
-    outb(port + 0, 0x01);           // 115200 baud (divisor 1)
+    outb(port + 0, 0x01);                 // 115200 baud (divisor 1)
     outb(port + 1, 0x00);
-    outb(port + UART_LCR, 0x03);    // 8n1
-    outb(port + UART_FCR, 0xC7);    // Enable FIFO
+    outb(port + UART_LCR, 0x03);          // 8n1
+    outb(port + UART_FCR, 0xC7);          // Enable FIFO
 }
 
 void serial_putc(uint16_t port, char c) 
@@ -20,7 +20,6 @@ void serial_putc(uint16_t port, char c)
 void serial_write(uint16_t port, const char* str) 
 {
     for (int i = 0; str[i] != '\0'; i++) {
-        // Handle newline for common serial monitors (Add CR if LF is found)
         if (str[i] == '\n') serial_putc(port, '\r');
         serial_putc(port, str[i]);
     }
