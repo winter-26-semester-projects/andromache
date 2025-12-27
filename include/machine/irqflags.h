cb86adf81a8123b7ad39e7b3c9e09e0871906a8a
sherif6931 <sherif6931@github.com>
2025-12-28 01:17:54 +0200

    feat: add asm headers, page table, irq, tss_struct

---
+++ include/machine/irqflags.h
@@ -0,0 +1,55 @@
+#ifndef _IRQFLAGS_H_
+#define _IRQFLAGS_H_
+
+#include<stdint.h>
+#include "barrier.h"
+
+static inline void enable(void) 
+{
+	__asm__ volatile("sti"
+			: : : "memory"
+			);
+}
+
+static inline void disable(void)
+{
+	__asm__ volatile("cli"
+		: : : "memory"
+		);
+}
+
+static inline unsigned long save_flags(void)
+{
+	unsigned long flags;
+
+	__asm__ volatile("pushfd ; pop %0"
+		: "=g"(flags) : : "memory"
+		);
+
+	return flags;
+}
+
+static inline void restore_flags(unsigned long flags)
+{
+	__asm__ volatile("push %0 ; popfd"
+		: : "g"(flags) : "memory"
+		);
+}
+
+static inline void save_irq_state(unsigned long *flags)
+{
+	*flags = save_flags();
+	
+	disable();
+	barrier();
+}
+
+static inline void restore_irq_state(unsigned long *flags)
+{
+	barrier();
+
+	restore_flags(*flags);
+}
+
+#endif /* _IRQFLAGS_H_ */
+
