ef16027f9ea0f1905a9dd9b386988cf75b8fbff2
youssefkaiser <youyou.kaiser@gmail.com>
2025-12-27 05:21:08 +0200

    implemented FCFS

--- src/modules/sched_algos/sched_algos.c
+++ src/modules/sched_algos/sched_algos.c
@@ -18,6 +18,30 @@ void sched_enqueue_task(struct task_struct *task)
     list_add_tail(&task->run_list, &ready_queue);
 }
 
+// FCFS implementation
+
+struct task_struct *sched_pick_next_task(sched_policy_t policy)
+{
+    struct task_struct *task;
+
+    if (list_empty(&ready_queue))
+        return NULL;
+
+    switch (policy)
+    {
+    case FCFS:
+        task = list_first_entry(
+            &ready_queue,
+            struct task_struct,
+            run_list);
+        list_del(&task->run_list);
+        task->state = RUNNING;
+        return task;
+    default:
+        return NULL;
+    }
+}
+
 // check for any ready task
 int sched_has_ready_tasks(void)
 {
