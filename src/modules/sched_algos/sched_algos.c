837ba664255d0108d125a7f4c664323aa106b17d
youssefkaiser <youyou.kaiser@gmail.com>
2025-12-27 05:20:29 +0200

    implemented base functions

--- src/modules/sched_algos/sched_algos.c
+++ src/modules/sched_algos/sched_algos.c
@@ -1,6 +1,25 @@
-#include <kernel/sched_algos.h>
+#include "../../../include/kernel/sched_algos.h"
 #include "private/_sched_algos.h"
+#include <stddef.h>
 
-void sched_algos_init(void) {
-    (void)sizeof(struct sched_algos_internal);
+struct list_head ready_queue;
+
+void sched_algos_init(void)
+{
+    INIT_LIST_HEAD(&ready_queue);
+}
+
+void sched_enqueue_task(struct task_struct *task)
+{
+    if (!task)
+        return;
+
+    task->state = READY;
+    list_add_tail(&task->run_list, &ready_queue);
+}
+
+// check for any ready task
+int sched_has_ready_tasks(void)
+{
+    return !list_empty(&ready_queue);
 }
