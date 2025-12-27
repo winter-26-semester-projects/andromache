9d303e1638580833d58cccaf323297fdd3642390
youssefkaiser <youyou.kaiser@gmail.com>
2025-12-27 02:55:56 +0200

    added basic task_struct just enough to serve sched_algos

--- include/kernel/task.h
+++ include/kernel/task.h
@@ -1,5 +1,6 @@
 #ifndef _KERNEL_TASK_H
 #define _KERNEL_TASK_H
+#include <linux/list.h>
 
 void task_init(void);
 
@@ -19,4 +20,21 @@ struct cpu_context
     void *sp;
     void *pc;
 };
+
+struct task_struct
+{
+    /*Basic standard task structure*/
+    int pid;
+    task_state_t state;
+    struct cpu_context context; // placeholder for cpu context for future implementation
+
+    /*Actuall sched algos related metadata*/
+    int priority;
+    int time_slice;
+    int remaining_time;
+    int burst_time;
+
+    struct list_head ready_queue;
+} task_t;
+
 #endif /* _KERNEL_TASK_h */
