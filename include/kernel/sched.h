74a43c369f58636dc322c76f6d06e06062ec3343
youssefkaiser <youyou.kaiser@gmail.com>
2025-12-30 21:31:08 +0200

    createed basic sched.h

--- include/kernel/sched.h
+++ include/kernel/sched.h
@@ -1,6 +1,14 @@
 #ifndef _KERNEL_SCHED_H_
 #define _KERNEL_SCHED_H_
+#include "kernel/task.h"
 
+/*called once during kernel boot to initialize the scheduler*/
 void sched_init(void);
+
+/*min scheduler function*/
+void schedule(void);
+
+/*voluntarily yield the CPU*/
+void yield(void);
 
 #endif /* _KERNEL_SCHED_H_ */
