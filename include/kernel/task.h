d5d2a86f392002f6909f664cc65ad6fc94d4fa04
sherif6931 <sherif6931@github.com>
2025-12-30 17:24:12 +0200

    got rid of weird version control msgs

--- include/kernel/task.h
+++ include/kernel/task.h
@@ -7,16 +7,7 @@
 /**
  * I shall henceforth, forward-declare a great many declarations...
  * */
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-enum pd_state { _=0,};
-=======
 enum pd_state {_=0,};
->>>>>>> 09bcd267cd5c15384bcb0c1871c3e700763a0037
-=======
-enum pd_state {_=0,};
->>>>>>> 09bcd267cd5c15384bcb0c1871c3e700763a0037
 struct mm_struct;
 
 struct task_struct {
