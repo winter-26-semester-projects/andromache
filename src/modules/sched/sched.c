427ac4ca106ed4fd84948402742abd4768d367d0
youssefkaiser <youyou.kaiser@gmail.com>
2025-12-30 21:59:09 +0200

    implemented global states and placeholder context_switch

--- src/modules/sched/sched.c
+++ src/modules/sched/sched.c
@@ -1,6 +1,16 @@
 #include <kernel/sched.h>
 #include "private/_sched.h"
+#include "kernel/sched_algos.h"
+#include "kernel/task.h"
+#include <stdint.h>
 
-void sched_init(void) {
-    (void)sizeof(struct sched_internal);
+/*Global states*/
+struct task_struct *current;
+struct task_struct idle_task;
+sched_policy_t curr_sched_policy = FCFS;
+
+/*Again, justa a placeholder nothing fancy*/
+void context_switch(struct task_struct *prev, struct task_struct *next)
+{
+    current = next;
 }
