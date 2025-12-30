d23620d7cfab63593f63a5b93a6abc61f981be67
youssefkaiser <youyou.kaiser@gmail.com>
2025-12-30 21:31:19 +0200

    created basic _sched.h

--- src/modules/sched/private/_sched.h
+++ src/modules/sched/private/_sched.h
@@ -1,8 +1,21 @@
 #ifndef _SCHED_PRIV_H_
 #define _SCHED_PRIV_H_
+#include "kernel/task.h"
+#include "kernel/sched_algos.h"
 
-struct sched_internal {
-    int placeholder;
-};
+/*currently running task*/
+extern struct task_struct *current;
+
+/*idle task (when no ready task is available)*/
+extern struct task_struct *idle_task;
+
+/*active sched policy*/
+extern sched_policy_t curr_sched_policy;
+
+/*
+placeholder context switch for future implementation
+ik it doesn't make any sense but just to build sched skeleton
+*/
+void context_switch(struct task_struct *prev, struct task_struct *next);
 
 #endif /* _SCHED_PRIV_H_ */
