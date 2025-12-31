72e5322ac5e2c71c0f6367c8b54a59f207e92f23
youssefkaiser <youyou.kaiser@gmail.com>
2026-01-01 00:58:15 +0200

    added task state updating, added RR_QUANTUM, added time based preemption

--- src/modules/sched/sched.c
+++ src/modules/sched/sched.c
@@ -3,11 +3,12 @@
 #include "kernel/sched_algos.h"
 #include "kernel/task.h"
 #include <stdint.h>
+#include "../../../include/kernel/time.h"
 
+#define RR_QUANTUM 5
 /*Global states*/
 struct task_struct *current;
 struct task_struct idle_task;
-sched_policy_t curr_sched_policy = FCFS;
 
 /*Again, justa a placeholder nothing fancy*/
 void context_switch(struct task_struct *prev, struct task_struct *next)
@@ -33,7 +34,10 @@ void sched_init(void)
     idle_task.pid = 0;
     idle_task.state = RUNNING;
     idle_task.entry = idle_task_func;
+    idle_task.policy = FCFS;
     current = &idle_task;
+
+    INIT_LIST_HEAD(&idle_task.run_list);
 }
 
 /*main scheduler*/
@@ -42,13 +46,27 @@ void schedule(void)
     struct task_struct *next;
 
     next = sched_pick_next_task(curr_sched_policy);
+
+    /* no task is ready */
     if (!next)
         next = &idle_task;
 
-    if (current != next)
-    {
-        context_switch(current, next);
-    }
+    /* no switch needed */
+    if (current == next)
+        return;
+
+    /* update task states */
+    if (current && current != &idle_task)
+        current->state = READY;
+
+    next->state = RUNNING;
+
+    /* reset RR quantum */
+    if (next->policy == RR)
+        next->time_slice = RR_QUANTUM;
+
+    context_switch(current, next);
+    current = next;
 }
 
 /*voluntary yield*/
@@ -60,4 +78,29 @@ void yield(void)
         sched_enqueue_task(current);
     }
     schedule();
+}
+
+void sched_tick(void)
+{
+    /*never preempt idle task*/
+    if (!current || current == &idle_task)
+        return;
+
+    switch (current->policy)
+    {
+    case RR:
+        current->time_slice--;
+        if (current->time_slice <= 0)
+        {
+            current->time_slice = RR_QUANTUM;
+            yield();
+        }
+        break;
+    case SDN:
+        current->remaining_time--;
+
+    /*HPF is not timer based preemptive and FCFS is non-preemptive */
+    default:
+        break;
+    }
 }
